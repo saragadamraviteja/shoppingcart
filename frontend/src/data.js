a0807ed21124b664cbe075718306c4b78b437ebe
saragadamraviteja <saragadamraviteja@gmai.com>
2020-11-21 14:00:14 +0530

    commit

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products:[
+        {
+        _id:'1',
+        name: 'T shirts',
+        category: 'Shirts',
+        image:'/images/iiii.jpg',
+        price: 600,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Long shirts',
+        category: 'Shirts',
+        image:'/images/iiii.jpg',
+        price: 600,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 6
+    },
+    {
+        _id:'3',
+        name: 'short shirts',
+        category: 'Pants',
+        image:'/images/iiii.jpg',
+        price: 600,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 7
+    },
+    {
+        _id:'4',
+        name: 'short shirts',
+        category: 'Pants',
+        image:'/images/iiii.jpg',
+        price: 600,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 7
+    }
+]
+}
